a7d3b18ac462fd7c5dbc97db925259d0066e4669
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-02-08 10:24:05 +0100

    Initial Commit

---
+++ menuRadio/menuRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuRadioApp.swift
+//  menuRadio
+//
+//  Created by Vitaliy Sharovatov on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct menuRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ menuRadioTests/menuRadioTests.swift
@@ -0,0 +1,17 @@
+//
+//  menuRadioTests.swift
+//  menuRadioTests
+//
+//  Created by Vitaliy Sharovatov on 08.02.2025.
+//
+
+import Testing
+@testable import menuRadio
+
+struct menuRadioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
